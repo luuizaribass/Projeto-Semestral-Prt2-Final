252b8e7c54f66f0b6ac3d81e0eeec79d5a5f8734
luuizaribass <133365362+luuizaribass@users.noreply.github.com>
2023-12-08 11:57:08 -0300

    .
    .

--- redireciona.js
+++ redireciona.js
@@ -1,13 +1,35 @@
-// Função de redirecionamento
-function redirecionarSeLogado() {
-    // Suponha que você tenha alguma lógica para verificar se o usuário está logado
-    var usuarioLogado = true; // Substitua isso pela sua lógica real
+// // Função de redirecionamento
+// function redirecionarSeLogado() {
+//     // Suponha que você tenha alguma lógica para verificar se o usuário está logado
+//     var usuarioLogado = true; // Substitua isso pela sua lógica real
 
-    if (usuarioLogado) {
-        // Se o usuário estiver logado, atualize o link do menu para "usuarioemuas.html"
-        var linkUsuario = document.getElementById('linkUsuario');
-        if (linkUsuario) {
-            linkUsuario.href = 'usuarioemaus.html';
-        }
+//     if (usuarioLogado) {
+//         // Se o usuário estiver logado, atualize o link do menu para "usuarioemuas.html"
+//         var linkUsuario = document.getElementById('linkUsuario');
+//         if (linkUsuario) {
+//             linkUsuario.href = 'usuarioemaus.html';
+//         }
+//     }
+// }
+
+const login = document.querySelector('.login')
+
+login.addEventListener('click', function () {
+    if (localStorage.getItem("userData")) {
+        window.location.href = "usuarioemaus.html";
+        login.setAttribute(href, "usuarioemaus.html")
+    } else {
+        window.location.href = "loginemaus.html"
+        login.setAttribute(href, "loginemaus.html")
+    }
+})
+
+function vai(){
+    if (localStorage.getItem("userData")) {
+        window.location.href = "usuarioemaus.html";
+        login.setAttribute(href, "usuarioemaus.html")
+    } else {
+        window.location.href = "loginemaus.html"
+        login.setAttribute(href, "loginemaus.html")
     }
 }
